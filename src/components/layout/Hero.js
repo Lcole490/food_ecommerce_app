0bda94439e29c28edf9da9f142d0548d79eeca4f
Lcole.engr <lcole.engr@gmail.com>
2024-07-12 15:18:03 -0400

    updated hero with columns and added to globals.css

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,13 +1,18 @@
+
 import Image from "next/image";
 import Right from "@/components/icons/Right"
+
+
 
 export default function Hero(){
 
     return(
-        <section className="grid grid-cols-2">
+        <section className="hero">
             <div className ="py-12">
-            <h1 className="text-4xl font-semibold">The Office Hotspot for quick meals</h1>
-            <p className="my-4 text-gray-500"> The Nook is your solution for a quick fix</p>
+            <h1 className="text-4xl font-semibold">The Office Hotspot <br /> for quick <br />
+            <span className="text-primary">Delicious Meals</span>
+            </h1>
+            <p className="my-4 text-gray-500 text-sm"> The Nook is your solution for a quick fix</p>
             <div className="flex gap-4 text-sm">
                 <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full ">Order Now <Right /> </button>
                 <button className="flex gap-2 py-2 text-gray-600 font-semibold">Learn More <Right /></button>
@@ -16,7 +21,7 @@ export default function Hero(){
             
 
         <div className="relative">
-            <Image src={'/coffee.png'} layout={'fill'}
+            <Image src={'/breakfast.png'} layout={'fill'}
             objectFit={'contain'} alt={'coffee'} />
         </div>
         </section>
