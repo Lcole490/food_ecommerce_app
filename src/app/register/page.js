8fac8f0f79d376f5c4c7621315538fdec07793cf
Lcole.engr <lcole.engr@gmail.com>
2024-07-14 02:32:30 -0400

    added bcrypt to hash passwords and users are able to register to the database manually

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -12,20 +12,32 @@ export default function RegisterPage(){
 
     const [userCreated, setUserCreated] = useState(false);
     const [creatingUser, setCreatingUser] = useState(false);
+    const [error, setError] = useState(false);
 
 
     async function handleFormSubmit(ev){
         ev.preventDefault();
 
         setCreatingUser(true);
-
-        await fetch('/api/register', {
+        setError(false);
+        setUserCreated(false);
+        
+        const response = await fetch('/api/register', {
             method: 'POST',
             body: JSON.stringify({email, password}),
             headers: {'Content-Type': 'application/json'}
         });
 
+        if(response.ok){
+            setUserCreated(true);
+        }
+        else{
+            setError(true);
+        }
         setCreatingUser(false);
+        
+
+    
     }
 
     return(
@@ -41,6 +53,14 @@ export default function RegisterPage(){
                 Registration Complete. <Link className="underline" href="/login">Login</Link> to Continue...
             </div>
         )}
+
+
+        {error && (
+            <div className="my-4 text-center">
+                An error has occurred. Please try again...
+            </div>
+        )}
+
 
         <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
             <input type="text" placeholder="email"  value={email} onChange={ev => setEmail(ev.target.value)} disabled={creatingUser}/>
@@ -60,6 +80,10 @@ export default function RegisterPage(){
           Login with Google 
           
         </button>
+
+        <div className="text-center my-4 text-gray-500 border-t pt-4">
+            Existing account? <Link className="underline" href={"/login"}>Login Here &raquo;</Link>
+        </div>
 
         </form>
 
